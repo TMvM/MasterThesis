3618c0e839fb9254a76dfa6ebade7c0a2cfa7ac6
Tom van Manen <tom.vanmanen@gmail.com>
2021-12-08 09:01:37 +0100

    Stock prices per T, returns per all.

--- Master Thesis 2.R
+++ Master Thesis 2.R
@@ -2,49 +2,169 @@ library(tidyverse)
 library(here)
 library(tidyquant)
 
-rdsa <- tq_get("RDS-A",                    
+rdsa_all <- tq_get("RDS-A",                    
+                 from = '2019-08-22',
+                 to = "2020-06-25",
+                 get = "stock.prices")
+
+rdsa_t0 <- tq_get("RDS-A",                    
+                  from = '2019-08-22',
+                  to = "2020-04-25",
+                  get = "stock.prices")
+
+rdsa_t1 <- tq_get("RDS-A",                    
+                  from = '2020-04-26',
+                  to = "2020-05-25",
+                  get = "stock.prices")
+
+rdsa_t2 <- tq_get("RDS-A",                    
+                  from = '2020-05-27',
+                  to = "2020-06-25",
+                  get = "stock.prices")
+
+bp_all <- tq_get("BP",                    
                from = '2019-08-22',
                to = "2020-06-25",
                get = "stock.prices")
 
-bp <- tq_get("BP",                    
+bp_t0 <- tq_get("BP",                    
+                  from = '2019-08-22',
+                  to = "2020-04-25",
+                  get = "stock.prices")
+
+bp_t1 <- tq_get("BP",                    
+                  from = '2020-04-26',
+                  to = "2020-05-25",
+                  get = "stock.prices")
+
+bp_t2 <- tq_get("BP",                    
+                  from = '2020-05-27',
+                  to = "2020-06-25",
+                  get = "stock.prices")
+
+totalenergies_all <- tq_get("TTE",                    
                from = '2019-08-22',
                to = "2020-06-25",
                get = "stock.prices")
 
-totalenergies <- tq_get("TTE",                    
+totalenergies_t0 <- tq_get("TTE",                    
+                from = '2019-08-22',
+                to = "2020-04-25",
+                get = "stock.prices")
+
+totalenergies_t1 <- tq_get("TTE",                    
+                from = '2020-04-26',
+                to = "2020-05-25",
+                get = "stock.prices")
+
+totalenergies_t2 <- tq_get("TTE",                    
+                from = '2020-05-27',
+                to = "2020-06-25",
+                get = "stock.prices")
+
+exxonmobil_all <- tq_get("XOM",                    
                from = '2019-08-22',
                to = "2020-06-25",
                get = "stock.prices")
 
-exonnmobil <- tq_get("XOM",                    
+exxonmobil_t0 <- tq_get("XOM",                    
+                from = '2019-08-22',
+                to = "2020-04-25",
+                get = "stock.prices")
+
+exxonmobil_t1 <- tq_get("XOM",                    
+                from = '2020-04-26',
+                to = "2020-05-25",
+                get = "stock.prices")
+
+exxonmobil_t2 <- tq_get("XOM",                    
+                from = '2020-05-27',
+                to = "2020-06-25",
+                get = "stock.prices")
+
+conoco_all <- tq_get("COP",                    
                from = '2019-08-22',
                to = "2020-06-25",
                get = "stock.prices")
 
-conoco <- tq_get("COP",                    
+conoco_t0 <- tq_get("COP",                    
+                from = '2019-08-22',
+                to = "2020-04-25",
+                get = "stock.prices")
+
+conoco_t1 <- tq_get("COP",                    
+                from = '2020-04-26',
+                to = "2020-05-25",
+                get = "stock.prices")
+
+conoco_t2 <- tq_get("COP",                    
+                from = '2020-05-27',
+                to = "2020-06-25",
+                get = "stock.prices")
+
+chevron_all <- tq_get("CVX",                    
                from = '2019-08-22',
                to = "2020-06-25",
                get = "stock.prices")
 
-chevron <- tq_get("CVX",                    
-               from = '2019-08-22',
-               to = "2020-06-25",
-               get = "stock.prices")
+chevron_t0 <- tq_get("CVX",                    
+                from = '2019-08-22',
+                to = "2020-04-25",
+                get = "stock.prices")
 
-indu <- tq_get("XOI",                    
+chevron_t1 <- tq_get("CVX",                    
+                from = '2020-04-26',
+                to = "2020-05-25",
+                get = "stock.prices")
+
+chevron_t2 <- tq_get("CVX",                    
+                from = '2020-05-27',
+                to = "2020-06-25",
+                get = "stock.prices")
+
+indu_all <- tq_get("XOI",                    
                   from = '2019-08-22',
                   to = "2020-06-25",
                   get = "stock.prices")
 
-mrkt <- tq_get("NYA",                    
+indu_t0 <- tq_get("XOI",                    
+                from = '2019-08-22',
+                to = "2020-04-25",
+                get = "stock.prices")
+
+indu_t1 <- tq_get("XOI",                    
+                from = '2020-04-26',
+                to = "2020-05-25",
+                get = "stock.prices")
+
+indu_t2 <- tq_get("XOI",                    
+                from = '2020-05-27',
+                to = "2020-06-25",
+                get = "stock.prices")
+
+mrkt_all <- tq_get("NYA",                    
                from = '2019-08-22',
                to = "2020-06-25",
                get = "stock.prices")
 
+mrkt_t0 <- tq_get("NYA",                    
+                from = '2019-08-22',
+                to = "2020-04-25",
+                get = "stock.prices")
+
+mrkt_t1 <- tq_get("NYA",                    
+                from = '2020-04-26',
+                to = "2020-05-25",
+                get = "stock.prices")
+
+mrkt_t2 <- tq_get("NYA",                    
+                from = '2020-05-27',
+                to = "2020-06-25",
+                get = "stock.prices")
+
 # Calculate daily returns rdsa
 
-rdsa_daily_returns <- rdsa %>%
+rdsa_daily_returns <- rdsa_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
@@ -52,7 +172,7 @@ rdsa_daily_returns <- rdsa %>%
 
 # Calculate daily returns bp
 
-bp_daily_returns <- bp %>%
+bp_daily_returns <- bp_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
@@ -60,7 +180,7 @@ bp_daily_returns <- bp %>%
 
 # Calculate daily returns totalenergies
 
-totalenergies_daily_returns <- totalenergies %>%
+totalenergies_daily_returns <- totalenergies_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
@@ -68,7 +188,7 @@ totalenergies_daily_returns <- totalenergies %>%
 
 # Calculate daily returns exxonmobil
 
-exxonmobil_daily_returns <- exonnmobil %>%
+exxonmobil_daily_returns <- exxonmobil_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
@@ -76,7 +196,7 @@ exxonmobil_daily_returns <- exonnmobil %>%
 
 # Calculate daily returns conoco
 
-conoco_daily_returns <- conoco %>%
+conoco_daily_returns <- conoco_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
@@ -84,7 +204,7 @@ conoco_daily_returns <- conoco %>%
 
 # Calculate daily returns chevron
 
-chevron_daily_returns <- chevron %>%
+chevron_daily_returns <- chevron_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
@@ -92,7 +212,7 @@ chevron_daily_returns <- chevron %>%
 
 # Calculate daily returns industry
 
-industry_daily_returns <- indu %>%
+industry_daily_returns <- indu_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
@@ -100,7 +220,7 @@ industry_daily_returns <- indu %>%
 
 # Calculate daily returns market
 
-market_daily_returns <- mrkt %>%
+market_daily_returns <- mrkt_all %>%
   tq_transmute(select = adjusted,           # this specifies which column to select   
                mutate_fun = periodReturn,   # This specifies what to do with that column
                period = "daily",      # This argument calculates Daily returns
